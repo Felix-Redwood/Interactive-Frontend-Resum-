124efec0a933f613c5ff621749f54e891b6ba554
Ubuntu <ubuntu@ip-172-31-85-146.ec2.internal>
2020-02-16 23:33:31 +0000

    added to sendEmail function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,17 @@
-console.log("hello");
+function sendMail(contactForm) {
+    email,
+    js.send(
+        "gmail", "template_1", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+    return false;
+}
